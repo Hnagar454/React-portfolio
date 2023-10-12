01937f5475fe90250fdce9e58df162376beaac48
hnagar454 <hnagar454@gmail.com>
2023-10-12 11:18:14 +0530

    change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./Index.css"
 
 
 function Navbar(){
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
     return(
 <div className="header">
 <nav className="navbar navbar-expand-lg" style={{color:"white"}} >
@@ -29,6 +40,11 @@ function Navbar(){
       </li>
       <li className="nav-item h3 m-2">
         <a className="nav-link "  id="Contact" href="#">Contact Me</a>
+      </li>
+      <li>
+      <div className={` ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
       </li>
     </ul>
     
